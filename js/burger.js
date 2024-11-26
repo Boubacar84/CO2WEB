0fbc32bd7e6fd27105f0141b56de94fedc24a3a1
Omarhio <big-dz@hotmail.fr>
2024-11-26 16:28:14 +0100

    Docs: add & fix img

--- js/burger.js
+++ js/burger.js
@@ -1,8 +1,11 @@
-function toggleMenu() {
-    const navbar = document.querySelector('.navbar');
-    const burger = document.querySelector('.burger');
-    burger.addEventListener('click', () => {
-        navbar.classList.toggle('show-nav');
-    })
-}
-toggleMenu();
+document.addEventListener("DOMContentLoaded", () => {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const menu = document.querySelector(".menu");
+    const content = document.querySelector(".content"); // Contenu principal
+
+    menuToggle.addEventListener("click", () => {
+        menu.classList.toggle("active");
+        menuToggle.classList.toggle("active");
+        content.classList.toggle("blurred"); // Ajout d’un flou au contenu
+    });
+});
