97e5b066d12ee3ee5fe0e39ef19f4ef50712f261
Omarhio <big-dz@hotmail.fr>
2024-11-28 15:57:40 +0100

    Docs: add link burger js

--- js/burger.js
+++ js/burger.js
@@ -3,19 +3,19 @@ document.addEventListener("DOMContentLoaded", () => {
     const burgerMenu = document.querySelector(".burger-menu");
 
     burgerToggle.addEventListener("click", () => {
-        burgerMenu.classList.toggle("active");
         burgerToggle.classList.toggle("active");
+        burgerMenu.classList.toggle("active");
     });
 
-    // Optionnel : Fermer si on clique ailleurs
+    // Optionnel : Fermer le menu si on clique à l'extérieur
     document.addEventListener("click", (event) => {
         if (
             !burgerMenu.contains(event.target) &&
             !burgerToggle.contains(event.target) &&
             burgerMenu.classList.contains("active")
         ) {
-            burgerMenu.classList.remove("active");
             burgerToggle.classList.remove("active");
+            burgerMenu.classList.remove("active");
         }
     });
 });
